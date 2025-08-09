3ed7a13be41d93fc9a5677e8cf0c14b59ce0d409
Nexus <rodgiellamas@gmail.com>
2025-08-09 19:14:02 +0800

    Create Default Notification.lua

---
+++ Default Notification.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+Title = "Notification", -- Change This you want 
+Text = "My Title Here", -- Also Change this you want
+Duration = 5, -- Change Duration you want
+})
